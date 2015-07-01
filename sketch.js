0d17c4aaea69edd493b979a5c8ed17c7dd6b5514
mactkg <mactkg@gmail.com>
2015-07-02 02:15:17 +0900

    yo

--- sketch.js
+++ sketch.js
@@ -1,25 +1,31 @@
 var capture;
-var graphic;
+var graphics;
 var cnt;
 var speed;
 var speedSlider;
 var togglePlay;
 var bPlay = true;
 var firstDraw = true;
+var firstFrame = 0;
 
 function preload() {
   capture = createCapture(VIDEO);
+
+  // graphic to store webcam image
+  graphics = [];
+  for(var i=0; i<100; i++) {
+    graphics[i] = createGraphics(400, 300);
+  }
+  console.log(graphics.length);
 }
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(400, 300);
+  frameRate(30);
 
   // webcam
   capture.size(width, height);
-  capture.hide();
-
-  // graphic to store webcam image
-  graphic = createGraphics(width, height);
+  //capture.hide();
 
   // settings
   cnt = 0;
@@ -42,36 +48,45 @@ function draw() {
   speed = speedSlider.value();
 
   // grab an image
-  graphic.background(255);
-  graphic.image(capture, 0, 0, capture.width, capture.height);
+  graphics[firstFrame].background(255);
+  graphics[firstFrame].image(capture, 0, 0, capture.width, capture.height);
 
   if(bPlay) {
     if(firstDraw && capture.loadedmetadata) {
-      image(graphic, 0, 0);
+      image(graphics[firstFrame], 0, 0);
       firstDraw = false;
     } else {
-      graphic.loadPixels();
       loadPixels();
 
-      // update line
-      var d = pixelDensity;
-      for(var dy=0; dy<speed; dy++) {
-        for(var x=0; x<width; x++) {
-          for(var i=0; i<d; i++) {
-            for(var j=0; j<d; j++) {
-              var idx = 4*((cnt * d + j) * width * d + (x * d + i));
-              pixels[idx] = graphic.pixels[idx];
-              pixels[idx+1] = graphic.pixels[idx+1];
-              pixels[idx+2] = graphic.pixels[idx+2];
-              pixels[idx+3] = graphic.pixels[idx+3];
+      for(var t=0; t<graphics.length; t++) {
+        var tt = (t+firstFrame) % graphics.length;
+        graphics[tt].loadPixels();
+
+        // frame group
+        var a = height/graphics.length;
+        for(var w=0; w<a; w++) {
+          var y = (t*a + w) % height;
+
+          // copy the line
+          var d = pixelDensity;
+          for(var x=0; x<width; x++) {
+            for(var i=0; i<d; i++) {
+              for(var j=0; j<d; j++) {
+                var idx = 4*((y * d + j) * width * d + (x * d + i));
+                pixels[idx] = graphics[tt].pixels[idx];
+                pixels[idx+1] = graphics[tt].pixels[idx+1];
+                pixels[idx+2] = graphics[tt].pixels[idx+2];
+                pixels[idx+3] = graphics[tt].pixels[idx+3];
+              }
             }
           }
         }
-        cnt = (cnt+1)%height;
       }
 
       updatePixels();
       text(speed, 10, 10);
     }
   }
+
+  firstFrame = (firstFrame+1)%graphics.length;
 }
